90911645e5e799c14eac2b80441fd4a4bb57cd8a
diegwl <diegocastanlopes0210@outlook.com>
2023-12-18 10:16:19 -0300

    starting the game

--- app/network.py
+++ app/network.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 class Network:
     def __init__(self):
@@ -6,21 +7,21 @@ class Network:
         self.server = "10.21.60.248"
         self.port = 8000
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def get_pos(self):
-        return self.pos
+    def get_p(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
 import sys
+from player import Player
 
 server = "0.0.0.0"
 port = 8000
@@ -15,14 +16,7 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, Server Started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0, 0, 50, 50, (255, 0, 0)), Player(100, 100, 50, 50, (0, 0,255))]
 
 def threaded_client(conn, player):
     conn.send(str.encode(make_pos(pos[player])))
